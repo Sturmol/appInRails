c249306c1f48d5d3b2cb0e423a716f1c7e99527e
Sturmol <sturmol@hotmail.com>
2016-08-10 16:29:02 +0200

    Styled properly and search function added

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,11 @@ class StaticPagesController < ApplicationController
   def index
   end
   def landing_page
-  	redirect_to static_pages_index_path
-  	#@featured_product = Product.first
-  	#@product = Product.limit(5)
+  	#redirect_to static_pages_index_path
+  	@featured_product = Product.first
+  	@product = Product.limit(5)
+  end
+
+  def price_change
   end
 end

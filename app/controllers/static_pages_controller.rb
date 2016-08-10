a8e1868bde733680b643ceac16a7b024bc4a84b3
Sturmol <sturmol@hotmail.com>
2016-08-10 15:51:45 +0200

    Rerouted to about page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,8 @@ class StaticPagesController < ApplicationController
   def index
   end
   def landing_page
-  	@featured_product = Product.first
-  	@product = Product.limit(5)
+  	redirect_to static_pages_index_path
+  	#@featured_product = Product.first
+  	#@product = Product.limit(5)
   end
 end

2e5e8a9d66543b198a5ffbe058978a6a8eda17b7
Sturmol <sturmol@hotmail.com>
2016-10-09 12:10:06 +0200

    fixed

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,17 @@
 
 ActiveRecord::Schema.define(version: 20160812094403) do
 
-  create_table "orders", force: :cascade do |t|
+  create_table "comments", force: true do |t|
+    t.integer  "user_id"
+    t.text     "body"
+    t.integer  "rating"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "orders", force: true do |t|
     t.integer "user_id"
     t.integer "product_id"
     t.float   "total"
@@ -22,20 +32,35 @@ ActiveRecord::Schema.define(version: 20160812094403) do
   add_index "orders", ["product_id"], name: "index_orders_on_product_id"
   add_index "orders", ["user_id"], name: "index_orders_on_user_id"
 
-  create_table "products", force: :cascade do |t|
+  create_table "products", force: true do |t|
     t.string   "name"
     t.text     "description"
     t.string   "image_url"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "color"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.decimal  "price"
+    t.string   "colour"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.boolean  "admin",                  default: false, null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
